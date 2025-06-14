c14b7243348daa8439ad2209a80faecfc83b2fcd
Victor Muniz <munizzvr@gmail.com>
2025-06-14 20:59:10 -0300

    Ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '/', // ou './src' se seu index.html estiver em src
+  root: '.', // ou './src' se seu index.html estiver em src
   plugins: [react()],
   build: {
     outDir: 'dist'
